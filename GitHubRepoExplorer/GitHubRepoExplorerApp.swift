6452608ea16ebef4c8e2f94bf6b291525541a2ec
Meng Li <meng.li.nzdev@gmail.com>
2026-02-23 13:39:20 +1300

    project creation

---
+++ GitHubRepoExplorer/GitHubRepoExplorerApp.swift
@@ -0,0 +1,32 @@
+//
+//  GitHubRepoExplorerApp.swift
+//  GitHubRepoExplorer
+//
+//  Created by Terry Li on 23/02/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct GitHubRepoExplorerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
